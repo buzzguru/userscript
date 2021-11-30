410abdb820576a627d6ca4b1f1c901e96a31aee3
Igor Suvorov <hi@isuvorov.com>
2021-11-30 13:03:41 +0300

    feat: developing

--- userscript/mega.user.js
+++ userscript/mega.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
-// @name         Example
-// @version      0.1
-// @description  Script from a website
-// @author       You
-// @match        *
+// @name         Mega Userscript Helper 
+// @namespace    http://tampermonkey.net/
+// @version      0.0.4
+// @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/mega.user.js
+// @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/mega.user.js
+// @description  try to take over the world!
+// @author       buzzguru
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
+
 
 GM_xmlhttpRequest({
   method: 'GET',
