2c746965d5ef65f6a0d932aadd11d9b3ce012cc4
Igor Suvorov <hi@isuvorov.com>
2021-11-30 12:53:17 +0300

    feat: add script.js

---
+++ userscript/mega.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Example
+// @version      0.1
+// @description  Script from a website
+// @author       You
+// @match        *://*.example.com/*
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+  method: 'GET',
+  // from other domain than the @match one (.org / .com):
+  url: 'https://buzzguru.github.io/userscript/script.js',
+  onload: (ev) => {
+    const e = document.createElement('script');
+    e.innerText = ev.responseText;
+    document.head.appendChild(e);
+  },
+});
