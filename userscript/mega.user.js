9968b325cbe3b1e258cb1c5f3739146d788e37a6
Igor Suvorov <hi@isuvorov.com>
2021-11-30 13:31:46 +0300

    feat: developing

--- userscript/mega.user.js
+++ userscript/mega.user.js
@@ -1,19 +1,35 @@
 // ==UserScript==
 // @name         Mega Userscript Helper 
 // @namespace    http://tampermonkey.net/
-// @version      0.0.4
+// @version      0.0.5
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/mega.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/mega.user.js
 // @description  try to take over the world!
 // @author       buzzguru
-// @match        *://*/*
+
+// @match        *://*.appannie.com/*
+
+// @match        https://youtube.com/*
+// @match        https://*.youtube.com/*
+
+// @match        https://instagram.com/*
+// @match        https://*.instagram.com/*
+
+// @match        https://tiktok.com/*
+// @match        https://*.tiktok.com/*
+
+// @match        https://twitch.com/*
+// @match        https://*.twitch.com/*
+
+// @match        https://modash.io/*
+// @match        https://*.modash.io/*
+
 // @connect      buzzguru.github.io
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
 
-alert('before onload')
 GM_xmlhttpRequest({
   method: 'GET',
   // from other domain than the @match one (.org / .com):
@@ -22,6 +38,5 @@ GM_xmlhttpRequest({
     const e = document.createElement('script');
     e.innerText = ev.responseText;
     document.head.appendChild(e);
-    alert('onload')
   },
 });
