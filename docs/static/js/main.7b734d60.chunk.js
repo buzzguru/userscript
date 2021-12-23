1d502ba2b379c41cde04b16d2c04a508aade9c37
Vlados <vlados.pochta@gmail.com>
2021-12-23 18:05:41 +0400

    chore: release

--- docs/static/js/main.7b734d60.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@lskjs/cra"]=this["webpackJsonp@lskjs/cra"]||[]).push([[0],{123:function(s,c,e){"use strict";(0,e(88)(e(124)).default)();console.log("Hello UserScript")}},[[123,1,2]]]);
-//# sourceMappingURL=main.7b734d60.chunk.js.map
