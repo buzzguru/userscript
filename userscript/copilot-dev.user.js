1b93c27c8adbe0085c0489e66ae725cd56a3c52e
Alex Mazaev <nadymorigin@gmail.com>
2022-04-13 18:09:57 +0400

    chore: release

--- userscript/copilot-dev.user.js
+++ userscript/copilot-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BuzzGuru Copilot [DEV]
 // @namespace    http://tampermonkey.net/
-// @version      1.7.0
+// @version      1.8.0
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/copilot-dev.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/copilot-dev.user.js
 // @description  insights
