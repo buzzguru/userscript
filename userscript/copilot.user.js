2636d17ae3e31465f3045a71a035146648b14aa5
Ivan Matskevich <faxtalk666@gmail.com>
2022-04-25 14:30:33 +0300

    chore: release

--- userscript/copilot.user.js
+++ userscript/copilot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BuzzGuru Copilot
 // @namespace    http://tampermonkey.net/
-// @version      1.20.1
+// @version      1.21.3
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/copilot.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/copilot.user.js
 // @description  insights
