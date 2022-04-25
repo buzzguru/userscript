f58a274fb039eeea5d64435a4fcfc3085d9e202f
Igor Suvorov <hi@isuvorov.com>
2022-04-25 15:54:48 +0300

    chore(release): v1.25.0

--- userscript/copilot-staging.user.js
+++ userscript/copilot-staging.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BuzzGuru Copilot [STAGING]
 // @namespace    http://tampermonkey.net/
-// @version      1.24.0
+// @version      1.25.0
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/copilot-staging.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/copilot-staging.user.js
 // @description  insights
