7c0bb7d2f6b559e3e523f0788a90afb861e6a8bc
Ivan Matskevich <faxtalk666@gmail.com>
2022-04-29 12:26:15 +0300

    chore(release): v1.27.0

--- userscript/copilot-staging.user.js
+++ userscript/copilot-staging.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BuzzGuru Copilot [STAGING]
 // @namespace    http://tampermonkey.net/
-// @version      1.26.0
+// @version      1.27.0
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/copilot-staging.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/copilot-staging.user.js
 // @description  Influencers insights
