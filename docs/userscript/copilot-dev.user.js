755d605232932e7edd9abcb7638f7320216c1f45
Ivan Matskevich <faxtalk666@gmail.com>
2022-05-24 03:22:14 +0300

    chore(release): v1.44.0

--- docs/userscript/copilot-dev.user.js
+++ docs/userscript/copilot-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copilot [DEV]
 // @namespace    http://tampermonkey.net/
-// @version      1.43.0
+// @version      1.44.0
 // @downloadURL  https://github.com/buzzguru/userscript/raw/master/userscript/copilot-dev.user.js
 // @updateURL    https://github.com/buzzguru/userscript/raw/master/userscript/copilot-dev.user.js
 // @description  Influencers insights
