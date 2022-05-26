53396f27bf94b82ffaf8933bb3a2aef65a8fe961
Igor Suvorov <hi@isuvorov.com>
2022-05-26 14:54:51 +0300

    chore(release): v1.48.0

--- userscript/copilot-dev.user.js
+++ userscript/copilot-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copilot
 // @namespace    http://tampermonkey.net/
-// @version      1.47.0
+// @version      1.48.0
 // @downloadURL  https://userscript.buzz.guru/userscript/copilot-dev.user.js
 // @updateURL    https://userscript.buzz.guru/userscript/copilot-dev.user.js
 // @description  Web insights
--- userscript/copilot-local_dev.user.js
+++ userscript/copilot-local_dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copilot [LOCAL]
 // @namespace    http://tampermonkey.net/
-// @version      1.47.0
+// @version      1.48.0
 // @downloadURL  https://userscript.buzz.guru/userscript/copilot-local_dev.user.js
 // @updateURL    https://userscript.buzz.guru/userscript/copilot-local_dev.user.js
 // @description  Web insights
