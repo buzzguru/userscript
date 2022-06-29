af475d0aa1e72de055ac56910d73a6e4a48c57e7
Vlados <vlados.pochta@gmail.com>
2022-06-29 17:33:19 +0400

    chore(release): v2.7.0

--- userscript/copilot-dev.user.js
+++ userscript/copilot-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copilot
 // @namespace    http://tampermonkey.net/
-// @version      2.6.0
+// @version      2.7.0
 // @downloadURL  https://userscript.buzz.guru/userscript/copilot-dev.user.js
 // @updateURL    https://userscript.buzz.guru/userscript/copilot-dev.user.js
 // @description  Web insights
